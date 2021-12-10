e12d116abc2b18cd322fbb3ba21b03cea258787b
IanaKos <95440446+IanaKos@users.noreply.github.com>
2021-12-10 22:21:34 +0100

    removed console

--- src/app.js
+++ src/app.js
@@ -64,11 +64,9 @@ function displayForecast(response) {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "cc1560478390be017bb24b1b8bff1dcc";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
