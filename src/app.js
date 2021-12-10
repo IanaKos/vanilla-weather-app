1ed614cd3ee9ac71a64b6505e090033e01b96dfc
IanaKos <95440446+IanaKos@users.noreply.github.com>
2021-12-10 11:19:23 +0100

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -51,7 +51,18 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "cc1560478390be017bb24b1b8bff1dcc";
-let city = "Warsaw";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "cc1560478390be017bb24b1b8bff1dcc";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Warsaw");
